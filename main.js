73c4566b4b0db620ccc5bc7629be696dea4f0d8f
Bruyok Vlad <djkill911@gmail.com>
2021-08-14 20:30:18 +0300

    Notes app version 1.0

--- main.js
+++ main.js
@@ -1,17 +1,14 @@
-// const test = document.getElementById('test');
-// console.log(test.dataset.idNote);
-// [{id: '1', value: 'vlad', date: '12-12-2001:13:32}, {id: '2', value: 'kirill', date: '12-12-2001:13:32}]
-
 // ---- CREATE VARIABLES HERE ----
 const NOTE_KEY = 'notes';
-const textarea = document.querySelector('.textarea-create');
-const buttonAdd = document.querySelector('.add-button');
+const createTextarea = document.querySelector('.create-textarea');
+const changeTextarea = document.querySelector('.change-textarea');
+const buttonAdd = document.querySelector('#add-button');
+const buttonChange = document.querySelector('#save-button');
 const notesContainer = document.querySelector('.notes-container');
-let textAriaValue;
-
-
+const createTextareaContainer = document.querySelector('.create-textarea-container');
+const changeTextareaContainer = document.querySelector('.change-textarea-container');
+let textAriaValue = '';
 // --- END CREATE VARIABLES ---
-
 function createNotes() {
     let notesFromLocalStorage = localStorage.getItem(NOTE_KEY); // this returns string
     let parsedNotesFromString = JSON.parse(notesFromLocalStorage); // this returns object (Array)
@@ -24,6 +21,11 @@ function createNotes() {
 
 function saveTextAriaInput(event) {
     textAriaValue = event.target.value;
+    if (textAriaValue === ''){
+        buttonAdd.setAttribute('disabled', 'disabled');
+    } else{
+        buttonAdd.removeAttribute("disabled");
+    }
 }
 
 function addNoteToNotesSection(newNote) {
@@ -44,25 +46,65 @@ function addNoteToNotesSection(newNote) {
     noteContainer.appendChild(newButtonDelete);
     notesContainer.appendChild(noteContainer);
 
+    textElement.addEventListener('click', function (){
+        createTextareaContainer.style.display = 'none';
+        changeTextareaContainer.style.display = 'block';
+
+        changeTextarea.setAttribute('data-id-note', newNote.id);
+        changeTextarea.value = newNote.value;
+        textAriaValue = newNote.value;
+    });
+
     newButtonDelete.addEventListener('click', function (event) {
-        console.log(event.target.dataset.idNote);
-        // add logic here
+        const clickedIdNote = event.target.dataset.idNote;
+        for (let i = 0; i < notes.length; i++) {
+            if (notes[i].id === clickedIdNote) {
+                notes.splice(i, 1);
+            }
+        }
+        localStorage.setItem(NOTE_KEY, JSON.stringify(notes));
+        notesContainer.removeChild(noteContainer);
     });
 }
 
 function addNote() {
-    const newNote = {id: Math.random() + '', value: textAriaValue};
+    const newNote = {id: Math.random().toString(), value: textAriaValue};
 
     notes.push(newNote);
+    textAriaValue = '';
     localStorage.setItem(NOTE_KEY, JSON.stringify(notes));
+
+    createTextarea.value = '';
+    buttonAdd.setAttribute('disabled', 'disabled');
     addNoteToNotesSection(newNote);
+}
+
+function createNotesSection(notes) {
+    for (let i = 0; i < notes.length; i++) {
+        addNoteToNotesSection(notes[i]);
+    }
 }
 
 // ---- START LOGIC HERE ----
 const notes = createNotes();
+createNotesSection(notes);
 
-textarea.addEventListener('input', saveTextAriaInput)
-buttonAdd.addEventListener('click', createNote);
+createTextarea.addEventListener('input', saveTextAriaInput);
+changeTextarea.addEventListener('input', saveTextAriaInput);
 
+buttonAdd.addEventListener('click', addNote);
+buttonChange.addEventListener('click', function () {
+    createTextareaContainer.style.display = 'block';
+    changeTextareaContainer.style.display = 'none';
+    const changeNoteId = changeTextarea.dataset.idNote;
+    for (let i = 0; i < notes.length; i++) {
+        if (notes[i].id === changeNoteId) {
+            notes[i].value = textAriaValue;
+        }
+    }
+    const noteToChange = document.querySelector(`.note[data-id-note="${changeNoteId}"]`);
+    noteToChange.innerHTML = textAriaValue;
+    localStorage.setItem(NOTE_KEY, JSON.stringify(notes));
+});
 
 // --- END START LOGIC ---
